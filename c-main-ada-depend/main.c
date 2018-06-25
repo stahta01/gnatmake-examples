0b650feea95f35f74f39b415f99fd171e3a03e37
Dave Smith <dave.k.smith@gmail.com>
2018-06-25 14:49:01 -0500

    Created main.c C main executable

---
+++ c-main-ada-depend/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern void adainit();
+extern void adafinal();
+extern void ada_proc();
+
+int main(int argc, char* argv[])
+{
+  printf("Starting C main.\n");
+  adainit();
+  ada_proc();
+  adafinal();
+  printf("Ending C main.\n");
+}
