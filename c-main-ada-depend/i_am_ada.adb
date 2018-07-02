717a3686552df55b2ca3434603d081269240f595
Dave Smith <dave.k.smith@gmail.com>
2018-07-02 16:39:14 -0500

    minor output formatting change

--- c-main-ada-depend/i_am_ada.adb
+++ c-main-ada-depend/i_am_ada.adb
@@ -2,6 +2,6 @@ with Ada.Text_IO; use Ada.Text_IO;
 package body I_Am_Ada is
   procedure Ada_Procedure is
   begin
-    Put_Line("Hello, I am Ada code.");
+    Put_Line("  Hello, I am Ada code.");
   end Ada_Procedure;
 end I_Am_Ada;
