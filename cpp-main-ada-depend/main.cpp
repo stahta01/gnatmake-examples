226d7b22f0f07c6f5f4801ac0356451eaa1c32fd
Dave Smith <dave.k.smith@gmail.com>
2018-07-03 09:30:57 -0500

    Create main.cpp

---
+++ cpp-main-ada-depend/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+extern "C" void adainit();
+extern "C" void adafinal();
+extern "C" void ada_proc();
+
+int main()
+{
+  std::cout << "Starting C++ main." << std::endl;
+  adainit();
+  ada_proc();
+  adafinal();
+  std::cout << "Ending C++ main." << std::endl;
+}
