ad543cb6e78f6a76c9fa79523b37ec374812c009
Dave Smith <dave.k.smith@gmail.com>
2018-07-05 14:30:33 -0500

    Create i_am_ada_too.adb

---
+++ ada-main-big/src/ada-b/i_am_ada_too.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+package body I_Am_Ada_Too is
+  procedure Ada_Procedure_Too is
+  begin
+    Put_Line("  Hello, I am Ada code too.");
+  end Ada_Procedure_Too;
+end I_Am_Ada_Too;
