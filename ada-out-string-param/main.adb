fb625cc5a8dc7a18f75528c533718910e60a882d
Dave Smith <dave.k.smith@gmail.com>
2018-12-08 14:07:25 -0600

    demonstrate Ada out String parameter

---
+++ ada-out-string-param/main.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+  procedure I_Return_String(S : out String)
+  begin
+    S := "Hellew, I am an Ada string.";
+  end I_Return_String;
+
+  Str : String;
+begin
+  I_Return_String(Str);
+  Put_Line("Output string is here ==>" & Str & "<==");
+end Main;
+
