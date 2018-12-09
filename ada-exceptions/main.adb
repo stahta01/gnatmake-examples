2610db30fed129ed2abde27e680867edc978674d
Dave Smith <dave.k.smith@gmail.com>
2018-12-09 17:31:35 -0600

    add more CONSTRAINT_ERROR examples

--- ada-exceptions/main.adb
+++ ada-exceptions/main.adb
@@ -2,16 +2,16 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
 begin
-
+  -- Guide:  http://archive.adaic.com/standards/83lrm/html/lrm-11-01.html
   declare
     PP : Positive;
     P2 : Positive;
   begin
     P2 := 5;
-    PP := 3 - P2;
+    PP := 3 - P2;  -- intentionally violate Positive range constraint
   exception
     when Constraint_Error =>
-      Put_Line("HANDLED: Assign negative to Positive causes Constraint_Error");
+      Put_Line("HANDLED: violate range constraint gives Constraint_Error");
   end;
 
   declare
@@ -19,10 +19,44 @@ begin
     PP : Positive;
   begin
     PP := 5;
-    SS(PP) := 'X';
+    SS(PP) := 'X';  -- intentionally violate SS's index constraint
   exception
     when Constraint_Error =>
-      Put_Line("HANDLED: array index out of bounds causes Constraint_Error");
+      Put_Line("HANDLED: violate index constraint gives Constraint_Error");
+  end;
+
+  declare
+    type Buffer(Size : Positive := 4) is
+      record
+        Value : String(1..Size);
+      end record;
+    BB : Buffer;  -- accept default Size
+  begin
+    BB.Value(5) := 'Y';  -- intentionally violate BB's discriminant constraint
+  exception
+    when Constraint_Error =>
+      Put_Line("HANDLED: violate discriminant constraint gives"
+               & " Constraint_Error");
+  end;
+
+  declare
+    type Liquid is (None, Water);
+    type Glass (Contents : Liquid) is
+      record
+        Weight : Positive;
+      case Contents is
+        when Water =>
+          Ounces : Positive;
+      end case;
+    end record;
+    GG : Glass(None);
+  begin
+    GG.Ounces := 5;  -- intentionally try to use a record component that does
+                     --    not exist for GG's "None" discriminant value
+  exception
+    when Constraint_Error =>
+      Put_Line("HANDLED: violate variant record component existance gives"
+               & " Constraint_Error");
   end;
 
 end Main;
