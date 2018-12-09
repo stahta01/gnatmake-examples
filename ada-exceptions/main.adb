917650b45b9ecf68fda9c08df7e44289cde1382f
Dave Smith <dave.k.smith@gmail.com>
2018-12-09 16:28:09 -0600

    check if runtime (not declarative) assignment fools compiler

--- ada-exceptions/main.adb
+++ ada-exceptions/main.adb
@@ -1,4 +1,4 @@
-with Ada.Calendar;
+--with Ada.Calendar;
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
@@ -6,11 +6,14 @@ begin
 
   declare
     PP : Positive;
-    Now : Ada.Calendar.Time;
+--    Now : Ada.Calendar.Time;
+    P2 : Positive;
   begin
-    Now := Ada.Calendar.Clock;
+--    Now := Ada.Calendar.Clock;
     -- Year will obviously be bigger than 1920
-    PP := 1920 - Positive(Ada.Calendar.Year(Now));
+--    PP := 1920 - Positive(Ada.Calendar.Year(Now));
+    P2 := 5;
+    PP := 3 - P2;
   exception
     when Constraint_Error =>
       Put_Line("Assigning a negative to a Positive causes Constraint_Error");
